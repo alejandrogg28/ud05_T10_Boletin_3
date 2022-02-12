a39649d01cfe70d1f761e8cc52f75c177c444fb9
Alejandro Garcia <alejandrogg28@gmail.com>
2022-02-12 16:42:05 +0100

    Ejercicio 9 Boletín 3 primer commit

---
+++ exercicio_9.php
@@ -0,0 +1,30 @@
+<?php
+//Realiza un programa que resuelva una ecuación de segundo grado (del tipo ax2 + bx + c = 0).
+
+header('Content-Type: text/html; charset=UTF-8');
+
+//$a=htmlspecialchars(trim(strip_tags($_REQUEST['a'])), ENT_QUOTES, "ISO-8859-1");
+//$b=htmlspecialchars(trim(strip_tags($_REQUEST['b'])), ENT_QUOTES, "ISO-8859-1");
+//$c=htmlspecialchars(trim(strip_tags($_REQUEST['c'])), ENT_QUOTES, "ISO-8859-1");
+
+$a = $_POST['a'];
+$b = $_POST['b'];
+$c = $_POST['c'];
+
+$neg = -1;
+
+$menosb = $b * $neg;
+$oper1 = pow($b, 2);
+$oper2 = 4 * $a * $c;
+$resta = $oper1 - $oper2;
+$raiz =  sqrt ( $resta);
+$dos_a = 2 * $a;
+
+$resultado1 = ($menosb + $raiz) / $dos_a;
+$resultado2 = ($menosb - $raiz) / $dos_a;
+
+echo "el valor de x1 es: ".$resultado1." <br />";
+echo "el valor de x2 es: ".$resultado2." <br />";
+?>
+
+
