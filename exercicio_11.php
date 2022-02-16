1f432fc55e41584047c824052733ca47c8bace81
Alejandro Garcia <alejandrogg28@gmail.com>
2022-02-16 14:57:15 +0100

    Ejercicios 4 5 y 11 Boletín 3

---
+++ exercicio_11.php
@@ -0,0 +1,19 @@
+<?php
+/*
+Escribe un programa que dada una hora determinada (horas y minutos), calcule los segundos que
+faltan para llegar a la medianoche
+*/
+//header('Content-Type: text/html; charset=UTF-8');
+
+$hora=$_POST['hora'];
+$minuto=$_POST['minuto'];
+
+if(!isset($hora) || !isset($minuto))
+echo "<p>Tes que introducir a hora e os minutos</p>";
+
+$segundos = (($hora * 3600) + ($minuto * 60));
+$segundos_medianoche = ((24 * 3600) - $segundos);
+
+echo "Quedan $segundos_medianoche segundos hasta la medianoche";
+
+?>
