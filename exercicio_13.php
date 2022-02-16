df5bd9383998c8841e18232bdb64cfd186a8e229
Alejandro Garcia <alejandrogg28@gmail.com>
2022-02-16 16:16:50 +0100

    Ejercicio 13 Boletín 3 segundo commit

--- exercicio_13.php
+++ exercicio_13.php
@@ -32,5 +32,62 @@ if ($num1 > $num2) {
     $num2 = $aux;
 }
 
-      echo "Los números introducidos ordenados de menor a mayor son $num1, $num2 y $num3.";    
+      echo "Los números introducidos ordenados de menor a mayor son $num1, $num2 y $num3.";
+      
+      
+
+
+/*
+Comparar los 2 primeros numeros introducidos
+si num1 mayor que num2
+entonces
+    almacenas el valor de num1 en variable almacen
+    $almacen = num1;
+
+    asignas el valor de $num2 a $num1 ya que el primer número tiene que ser el más pequeño
+    y la condición aquí es si num1 es mayor que num2
+    $num1 = $num2;
+
+    $almacen tendrá el valor de num2 porque es el número pequeño
+    $num2 = $almacen;
+
+Comparar el segundo y tercer número introducido
+si num2 es mayor que num3
+entonces
+
+hacemos lo mismo que en la otra condición, ya que el segundo número tiene que ser menor al tercero
+    $almacen = $num2;
+    $num2 = $num3;
+    $num3 = $almacen;
+
+Ahora $num3 tiene el valor que se introducirá en $num2 por lo que hay que recrear la primera condición
+para así poder ordenarlos
+si num1 es mayor que num2
+entonces
+    $almacen = $num1;
+    $num1 = $num2;
+    $num2 = $almacen;
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 ?>
