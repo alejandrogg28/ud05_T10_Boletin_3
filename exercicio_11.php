2c0fc28c8b61792ab79b6ffad8ce0ca13520b418
Alejandro Garcia <alejandrogg28@gmail.com>
2022-02-17 12:16:12 +0100

    Ejercicio 11 Boletín 3 segundo commit

--- exercicio_11.php
+++ exercicio_11.php
@@ -5,17 +5,19 @@ faltan para llegar a la medianoche
 */
 //header('Content-Type: text/html; charset=UTF-8');
 
-$hora=$_POST['hora'];
-$minuto=$_POST['minuto'];
 
-if(!isset($hora) || !isset($minuto))
-echo "<p>Tes que introducir a hora e os minutos</p>";
+if(isset($_POST['hora']) && isset($_POST['minuto']) && $_POST['hora']!="" && $_POST['minuto']!=""){
 
-else{
-$segundos = (($hora * 3600) + ($minuto * 60));
-$segundos_medianoche = ((24 * 3600) - $segundos);
+    $hora=$_POST['hora'];
+    $minuto=$_POST['minuto'];
+    
+    $segundos = (($hora * 3600) + ($minuto * 60));
+    $segundos_medianoche = ((24 * 3600) - $segundos);
+    
+    
+    echo "Quedan $segundos_medianoche segundos hasta la medianoche";
+}else{
+    echo "<p>Tes que introducir a hora e os minutos</p>";
 
-
-echo "Quedan $segundos_medianoche segundos hasta la medianoche";
 }
 ?>
